f73ad89e919230ac32c5ca52f05cca6d44f6925e
Rajashree Malvade <rajashreermalvade@gmail.com>
2017-02-12 12:13:14 +0530

    Show the list of English numbers on the Numbers screen

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> numbersList = new ArrayList<String>();
+
+        numbersList.add("One");
+        numbersList.add("Two");
+        numbersList.add("Three");
+        numbersList.add("Four");
+        numbersList.add("Five");
+        numbersList.add("Six");
+        numbersList.add("Seven");
+        numbersList.add("Eight");
+        numbersList.add("Nine");
+        numbersList.add("Ten");
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootview);
+
+        for(int i=0 ; i < numbersList.size(); i++){
+            TextView textView = new TextView(this);
+            textView.setText(numbersList.get(i));
+            rootView.addView(textView);
+        }
+
     }
 }
