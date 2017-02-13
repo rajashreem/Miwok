e4635e669d404b20b8a4d70729963f9a42b35642
Rajashree Malvade <rajashreermalvade@gmail.com>
2017-02-13 15:25:29 +0530

    Use custom WordAdapter to show the list of numbers

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,7 +2,6 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -27,8 +26,8 @@ public class NumbersActivity extends AppCompatActivity {
         numbersList.add(new Word("Nine", "Wo'e"));
         numbersList.add(new Word("Ten", "Na'aacha"));
 
-        ArrayAdapter<Word> numbersAdapater = new ArrayAdapter<Word>(this, R.layout.list_item, numbersList);
+        WordAdapter wordAdapater = new WordAdapter(this, numbersList);
         ListView listView = (ListView) findViewById(R.id.numbers_list_view);
-        listView.setAdapter(numbersAdapater);
+        listView.setAdapter(wordAdapater);
     }
 }
