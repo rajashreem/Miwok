fcfea875ed3dbdaaca9525f6cfb90ec64b404a6a
Rajashree Malvade <rajashreermalvade@gmail.com>
2017-02-15 12:27:06 +0530

    Add images to colors, family and numbers words

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -15,16 +15,16 @@ public class NumbersActivity extends AppCompatActivity {
 
         ArrayList<Word> numbersList = new ArrayList<Word>();
 
-        numbersList.add(new Word("One", "Lutti"));
-        numbersList.add(new Word("Two", "Otiiko"));
-        numbersList.add(new Word("Three", "Tolookosu"));
-        numbersList.add(new Word("Four", "Oyyisa"));
-        numbersList.add(new Word("Five", "Massaokka"));
-        numbersList.add(new Word("Six", "Temmokka"));
-        numbersList.add(new Word("Seven", "kenekaku"));
-        numbersList.add(new Word("Eight", "Kawinta"));
-        numbersList.add(new Word("Nine", "Wo'e"));
-        numbersList.add(new Word("Ten", "Na'aacha"));
+        numbersList.add(new Word("One", "Lutti", R.drawable.number_one));
+        numbersList.add(new Word("Two", "Otiiko", R.drawable.number_two));
+        numbersList.add(new Word("Three", "Tolookosu", R.drawable.number_three));
+        numbersList.add(new Word("Four", "Oyyisa", R.drawable.number_four));
+        numbersList.add(new Word("Five", "Massaokka", R.drawable.number_five));
+        numbersList.add(new Word("Six", "Temmokka", R.drawable.number_six));
+        numbersList.add(new Word("Seven", "kenekaku", R.drawable.number_seven));
+        numbersList.add(new Word("Eight", "Kawinta", R.drawable.number_eight));
+        numbersList.add(new Word("Nine", "Wo'e", R.drawable.number_nine));
+        numbersList.add(new Word("Ten", "Na'aacha", R.drawable.number_ten));
 
         WordAdapter wordAdapater = new WordAdapter(this, numbersList);
         ListView listView = (ListView) findViewById(R.id.word_list_view);
