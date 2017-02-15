6865313050c6e21a0c005f7b6ba90038e472a97f
Rajashree Malvade <rajashreermalvade@gmail.com>
2017-02-15 12:34:16 +0530

    Show the images for family, colors and number words

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -30,6 +31,9 @@ public class WordAdapter extends ArrayAdapter<Word>{
 
         TextView defaultTranslation = (TextView) listItemView.findViewById(R.id.default_text_view);
         defaultTranslation.setText(word.getDefaultTranslation());
+
+        ImageView wordImage = (ImageView) listItemView.findViewById(R.id.word_image);
+        wordImage.setImageResource(word.getImageResourceId());
 
         return listItemView;
     }
