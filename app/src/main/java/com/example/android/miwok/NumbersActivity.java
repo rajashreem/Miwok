126517ce7392ffcc0a8bdf6ddc766a7bd9f462a8
Rajashree Malvade <rajashreermalvade@gmail.com>
2017-02-25 11:19:37 +0530

    Minor renaming

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -24,20 +24,20 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
-        final ArrayList<Word> numbersList = new ArrayList<Word>();
+        final ArrayList<Word> words = new ArrayList<Word>();
 
-        numbersList.add(new Word("One", "Lutti", R.drawable.number_one, R.raw.number_one));
-        numbersList.add(new Word("Two", "Otiiko", R.drawable.number_two, R.raw.number_two));
-        numbersList.add(new Word("Three", "Tolookosu", R.drawable.number_three, R.raw.number_three));
-        numbersList.add(new Word("Four", "Oyyisa", R.drawable.number_four, R.raw.number_four));
-        numbersList.add(new Word("Five", "Massaokka", R.drawable.number_five, R.raw.number_five));
-        numbersList.add(new Word("Six", "Temmokka", R.drawable.number_six, R.raw.number_six));
-        numbersList.add(new Word("Seven", "Kenekaku", R.drawable.number_seven, R.raw.number_seven));
-        numbersList.add(new Word("Eight", "Kawinta", R.drawable.number_eight, R.raw.number_eight));
-        numbersList.add(new Word("Nine", "Wo'e", R.drawable.number_nine, R.raw.number_nine));
-        numbersList.add(new Word("Ten", "Na'aacha", R.drawable.number_ten, R.raw.number_ten));
+        words.add(new Word("One", "Lutti", R.drawable.number_one, R.raw.number_one));
+        words.add(new Word("Two", "Otiiko", R.drawable.number_two, R.raw.number_two));
+        words.add(new Word("Three", "Tolookosu", R.drawable.number_three, R.raw.number_three));
+        words.add(new Word("Four", "Oyyisa", R.drawable.number_four, R.raw.number_four));
+        words.add(new Word("Five", "Massaokka", R.drawable.number_five, R.raw.number_five));
+        words.add(new Word("Six", "Temmokka", R.drawable.number_six, R.raw.number_six));
+        words.add(new Word("Seven", "Kenekaku", R.drawable.number_seven, R.raw.number_seven));
+        words.add(new Word("Eight", "Kawinta", R.drawable.number_eight, R.raw.number_eight));
+        words.add(new Word("Nine", "Wo'e", R.drawable.number_nine, R.raw.number_nine));
+        words.add(new Word("Ten", "Na'aacha", R.drawable.number_ten, R.raw.number_ten));
 
-        WordAdapter wordAdapater = new WordAdapter(this, numbersList, R.color.category_numbers);
+        WordAdapter wordAdapater = new WordAdapter(this, words, R.color.category_numbers);
         ListView listView = (ListView) findViewById(R.id.word_list_view);
         listView.setAdapter(wordAdapater);
 
@@ -46,7 +46,7 @@ public class NumbersActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 releaseMediaPlayer();
 
-                mediaPlayer = MediaPlayer.create(view.getContext(), numbersList.get(position).getPronunciation());
+                mediaPlayer = MediaPlayer.create(view.getContext(), words.get(position).getPronunciation());
 
                 mediaPlayer.start();
 
