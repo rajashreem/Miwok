232c3df0e379e2c5f9accff8196bcd00a8f975cd
Rajashree Malvade <rajashreermalvade@gmail.com>
2017-02-27 16:44:29 +0530

    Show word swipeable categories with pager

--- app/src/main/java/com/example/android/miwok/CategoryFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryFragmentPagerAdapter.java
@@ -4,18 +4,37 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
-public class CategoryFragmentPagerAdapter extends FragmentPagerAdapter{
+public class CategoryFragmentPagerAdapter extends FragmentPagerAdapter {
     public CategoryFragmentPagerAdapter(FragmentManager fm) {
         super(fm);
     }
 
     @Override
     public Fragment getItem(int position) {
-        return null;
+        Fragment fragment = null;
+        switch (position) {
+            case 0: {
+                fragment = new NumbersFragment();
+                break;
+            }
+            case 1: {
+                fragment = new FamilyFragment();
+                break;
+            }
+            case 2: {
+                fragment = new ColorsFragment();
+                break;
+            }
+            case 3: {
+                fragment = new PhrasesFragment();
+                break;
+            }
+        }
+        return fragment;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return 4;
     }
 }
